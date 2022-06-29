56dd29f568268b971c313280f554fba6b51dceef
Aleksey <1862647@gmail.com>
2022-06-29 14:49:07 +0700

    add fibonacci algorithm

--- src/components/fibonacci-page/fibonacci-page.tsx
+++ src/components/fibonacci-page/fibonacci-page.tsx
@@ -1,10 +1,63 @@
-import React from "react";
-import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { FC, useState } from 'react';
+import FibonacciStyles from './fibonacci.module.css';
+import { Button } from '../ui/button/button';
+import { Circle } from '../ui/circle/circle';
+import { Input } from '../ui/input/input';
+import { SolutionLayout } from '../ui/solution-layout/solution-layout';
+import { nanoid } from 'nanoid';
 
-export const FibonacciPage: React.FC = () => {
-  return (
-    <SolutionLayout title="Последовательность Фибоначчи">
-      
-    </SolutionLayout>
-  );
+export const FibonacciPage: FC = () => {
+	const [fibNumbers, setFibNumbers] = useState<string[]>([]);
+	const [loader, setLoader] = useState<boolean>(false);
+	let interval: NodeJS.Timer;
+
+	const [value, setValue] = useState('');
+	const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setValue(e.target.value);
+	};
+
+	const algorithm = (n: number) => {
+		setLoader(true);
+		const fibArr: string[] = [];
+		let i = 1;
+		interval = setInterval(() => {
+			if (i <= 2) {
+				fibArr.push('1');
+				setFibNumbers([...fibArr]);
+			} else {
+				fibArr.push(`${+fibArr[i - 2] + +fibArr[i - 3]}`);
+				setFibNumbers([...fibArr]);
+			}
+
+			if (n === i - 1) {
+				setLoader(false);
+				clearInterval(interval);
+			}
+			i++;
+		}, 500);
+	};
+
+	const reverseHandler = () => algorithm(+value);
+
+	return (
+		<SolutionLayout title='Последовательность Фибоначчи'>
+			<div className={FibonacciStyles.container}>
+				<Input
+					onChange={onChangeInput}
+					isLimitText
+					type='number'
+					max={19}></Input>
+				<Button
+					text='Рассчитать'
+					onClick={reverseHandler}
+					disabled={+value > 19}
+					isLoader={loader}></Button>
+			</div>
+			<div className={FibonacciStyles.fibonacci} style={{justifyContent: fibNumbers.length < 10 ? 'center' : 'flex-start'}}>
+				{fibNumbers.map((number: string, i: number) => (
+					<Circle letter={number} key={nanoid()} index={i}></Circle>
+				))}
+			</div>
+		</SolutionLayout>
+	);
 };
--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,5 +1,5 @@
 import { FC, useState } from 'react';
-import StringStyles from './string.module.css'
+import StringStyles from './string.module.css';
 import { Button } from '../ui/button/button';
 import { Input } from '../ui/input/input';
 import { SolutionLayout } from '../ui/solution-layout/solution-layout';
@@ -9,61 +9,65 @@ import { ElementStates } from '../../types/element-states';
 import { swap } from '../../utils/utils';
 
 export const StringComponent: FC = () => {
-  type TSymbol = {
-    symbol: string,
-    state: ElementStates
-  }
+	type TSymbol = {
+		symbol: string;
+		state: ElementStates;
+	};
 
-  const [symbols, setSymbols] = useState<TSymbol[]>([])
-  const [loader, setLoader] = useState<boolean>(false)
-  let interval: NodeJS.Timer
+	const [symbols, setSymbols] = useState<TSymbol[]>([]);
+	const [loader, setLoader] = useState<boolean>(false);
+	let interval: NodeJS.Timer;
 
-  const [value, setValue] = useState('')
-  const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value)
-  }
-      
-    const algorithm = (elements: TSymbol[]) => {
-      setLoader(true)
-      setSymbols(elements)
-      let i = 0
-      let j = elements.length - 1
-      interval = setInterval(() => {
-        swap(elements, i, j)
-        elements[i].state = ElementStates.Modified
-        elements[i+1].state = ElementStates.Changing
-        elements[j].state = ElementStates.Modified
-        elements[j-1].state = ElementStates.Changing
-        setSymbols([...elements])
-        i++
-        j--
-        if (Math.floor(elements.length / 2) === i) {
-          elements[i].state = ElementStates.Modified
-          elements[i-1].state = ElementStates.Modified
-          setSymbols([...elements])
-          setLoader(false)
-          clearInterval(interval)
-        } 
-      }, 1000)      
-    }    
+	const [value, setValue] = useState('');
+	const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setValue(e.target.value);
+	};
 
-  const reverseHandler = () => {
-    const elements = value.split('').map(el => ({symbol: el, state: ElementStates.Default}))
-    algorithm(elements)
-  }
+	const algorithm = (elements: TSymbol[]) => {
+		setLoader(true);
+		setSymbols(elements);
+		let i = 0;
+		let j = elements.length - 1;
+		interval = setInterval(() => {
+			swap(elements, i, j);
+			elements[i].state = ElementStates.Modified;
+			elements[i + 1].state = ElementStates.Changing;
+			elements[j].state = ElementStates.Modified;
+			elements[j - 1].state = ElementStates.Changing;
+			setSymbols([...elements]);
+			i++;
+			j--;
+			if (Math.floor(elements.length / 2) === i) {
+				elements[i].state = ElementStates.Modified;
+				elements[i - 1].state = ElementStates.Modified;
+				setSymbols([...elements]);
+				setLoader(false);
+				clearInterval(interval);
+			}
+		}, 1000);
+	};
+
+	const reverseHandler = () => {
+		const elements = value
+			.split('')
+			.map((el) => ({ symbol: el, state: ElementStates.Default }));
+		algorithm(elements);
+	};
 
 	return (
 		<SolutionLayout title='Строка'>
 			<div className={StringStyles.container}>
 				<Input onChange={onChangeInput} isLimitText maxLength={11}></Input>
-				<Button text='Развернуть' onClick={reverseHandler} isLoader={loader}></Button>
+				<Button
+					text='Развернуть'
+					onClick={reverseHandler}
+					isLoader={loader}></Button>
 			</div>
-      <div className={StringStyles.string}>
-        {symbols.map(({symbol, state}: TSymbol) => (
-        <Circle letter={symbol} key={nanoid()} state={state} ></Circle>
-        ))}
-      </div>
+			<div className={StringStyles.string}>
+				{symbols.map(({ symbol, state }: TSymbol) => (
+					<Circle letter={symbol} key={nanoid()} state={state}></Circle>
+				))}
+			</div>
 		</SolutionLayout>
 	);
 };
-
